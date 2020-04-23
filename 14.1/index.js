554ed1ec59985ff148e9a81bb4f131a84785b833
Silvia_Gabrielyan <gabrielyan.silvia89@gmail.com>
2020-04-23 23:46:51 +0300

    homewor

--- 14.1/index.js
+++ 14.1/index.js
@@ -2,5 +2,6 @@
         for (let i = 0; i < squares.length; ++i) {
             squares[i].addEventListener("click", handler);
         };
-        [...document.getElementsByTagName('div')].forEach(e =>
-    e.addEventListener('click', ()=> e.innerText = ++e.innerText ) );
+        function handler(e) {
+            this.innerText = ++this.innerText;
+        }
