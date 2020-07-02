711b951361be61f2c56280a440455a412e72383d
Silvia <gabrielyan.silvia89@gmail>
2020-07-02 22:37:40 +0300

    new

--- new application/script.sj.js
+++ new application/script.sj.js
@@ -10,13 +10,33 @@ const personalMoviaDB = {
 	privat: false
 };
 
-const a = prompt('Which one your last film?', ''),
-	  b = prompt('What do you think abaut this?', ''),
-	  c = prompt('Which one your last film?', ''),
-	  d = prompt('What do you think abaut this?', '');
 
 
-personalMoviaDB.movies[a] = b;
-personalMoviaDB.movies[c] = d;
+
+
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Which one your last film?', ''),
+		  b = prompt('What do you think abaut this?', '');
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		 personalMoviaDB.movies[a] = b;
+		 console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+}
+
+if (personalMoviaDB.count < 10) {
+	console.log("You are whtaching a lot of films");
+} else if (personalMoviaDB.count >= 10 && personalMoviaDB.count < 30) {
+	console.log("You are good guy");
+} else if (personalMoviaDB.count >= 30) {
+	console.log("You are crazy");
+	} else {
+		console.log("False");
+	}
+
+
 
 console.log(personalMoviaDB);
+
